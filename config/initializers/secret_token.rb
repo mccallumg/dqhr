6ed9879573758d9791e4bd781329799ec11aef58
Glen McCallum <glen.mccallum@nextinformatics.com>
2012-04-14 23:21:04 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dqhr::Application.config.secret_token = 'fee123fcd221fe9652c7bfb1c2b019e685f78422f3938ff69a34db3eb210984af30460fe06bcacbbc2b639802a758e94001d0fbe244ba4428075031ac416c074'
